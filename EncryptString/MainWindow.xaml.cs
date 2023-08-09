0504e8485c5a7d8d1891803822175c61bb5a6734
Nguyễn Ngọc Cường <nguyenngoccuongls@outlook.com>
2023-08-09 23:49:54 +0700

    resource

--- EncryptString/MainWindow.xaml.cs
+++ EncryptString/MainWindow.xaml.cs
@@ -71,29 +71,49 @@ namespace EncryptString
             if (bt.Name == btnCopy_MD5.Name)
             {
                 Clipboard.SetText(tblk_MD5.Text, TextDataFormat.Text);
+                tblk_MD5.SelectAll();
+                statusItem1.Content = "MD5 text copied!";
             }
             else if (bt.Name == btnCopy_SHA1.Name)
             {
                 Clipboard.SetText(tblk_SHA1.Text, TextDataFormat.Text);
+                tblk_SHA1.SelectAll();
+                statusItem1.Content = "SHA1 text copied!";
+
             }
             else if (bt.Name == btnCopy_SHA256.Name)
             {
                 Clipboard.SetText(tblk_SHA256.Text, TextDataFormat.Text);
+                tblk_SHA256.SelectAll();
+                statusItem1.Content = "SHA256 text copied!";
             }
             else if (bt.Name == btnCopy_SHA384.Name)
             {
                 Clipboard.SetText(tblk_SHA384.Text, TextDataFormat.Text);
+                tblk_SHA384.SelectAll();
+                statusItem1.Content = "SHA384 text copied!";
             }
             else if (bt.Name == btnCopy_SHA512.Name)
             {
                 Clipboard.SetText(tblk_SHA512.Text, TextDataFormat.Text);
+                tblk_SHA512.SelectAll();
+                statusItem1.Content = "SHA512 text copied!";
             }
-
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txb_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            statusItem1.Content = "Key down: " + e.Key.ToString();
         }
     }
 }
