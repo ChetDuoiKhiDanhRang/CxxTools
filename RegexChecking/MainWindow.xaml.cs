0eee3e73ba025058d85451f09aae5fb47a42f876
Nguyễn Ngọc Cường <nguyenngoccuongls@outlook.com>
2023-08-12 01:11:11 +0700

    bb

--- RegexChecking/MainWindow.xaml.cs
+++ RegexChecking/MainWindow.xaml.cs
@@ -29,7 +29,24 @@ namespace RegexChecking
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            ObjRegex = new Regex(txbPattern.Text);
+            ObjRegex = new Regex(txbPattern.Text,
+                RegexOptions.None |
+                ((bool)ckbCompiled.IsChecked ? RegexOptions.Compiled : RegexOptions.None) |
+                ((bool)ckbCultureInvariant.IsChecked ? RegexOptions.CultureInvariant : RegexOptions.None) |
+                ((bool)ckbECMAScript.IsChecked ? RegexOptions.ECMAScript : RegexOptions.None) |
+                ((bool)ckbExplicitCapture.IsChecked ? RegexOptions.ExplicitCapture : RegexOptions.None) |
+                ((bool)ckbIgnoreCase.IsChecked ? RegexOptions.IgnoreCase : RegexOptions.None) |
+                ((bool)ckbIgnorePatternWhitespace.IsChecked ? RegexOptions.IgnorePatternWhitespace : RegexOptions.None) |
+                ((bool)ckbMultiline.IsChecked ? RegexOptions.Multiline : RegexOptions.None) |
+                ((bool)ckbRightToLeft.IsChecked ? RegexOptions.RightToLeft : RegexOptions.None) |
+                ((bool)ckbSingleline.IsChecked ? RegexOptions.Singleline : RegexOptions.None)
+            );
+
+            foreach (Match item in ObjRegex.Matches(txbContent.Text))
+            {
+                item.
+            }
+            
 
         }
 
