2e2e57dae07a356b24914ef7506b14eae9e7f4b5
ChetDuoiKhiDanhRang <58522857+ChetDuoiKhiDanhRang@users.noreply.github.com>
2023-09-29 15:19:43 +0700

    ggg

---
+++ WindowAndControl/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BaseTools;
+using BaseTools.Win32API_Objects;
+
+namespace WindowAndControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new Timer(50);
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        CursorInfo ci;
+
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            ci = BaseTools.WinAPI.GetCursorInfo();
+            txbLocation.Dispatcher.Invoke(() => txbLocation.Text = $"{{{ci.ToString()}}}");
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.Title = "Mouse Down!";
+        }
+
+        private void Window_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Title = "Mouse Up!";
+
+        }
+
+        public bool TimerStarted { get; set; }
+
+
+        private void btnTrackMouse_Click(object sender, RoutedEventArgs e)
+        {
+            if (TimerStarted)
+            {
+                timer.Stop();
+            }
+            else
+            {
+                timer.Start();
+            }
+            TimerStarted = !TimerStarted;
+        }
+    }
+}
